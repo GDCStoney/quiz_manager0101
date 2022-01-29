efbc3dc9cfff4e341ed88f20690cdc664a512193
gstoney2 <gstoney2@ford.com>
2022-01-29 11:52:43 +0000

    Initial load

---
+++ src/main/java/com/makers/quizmanager/QuizManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.makers.quizmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makers/quizmanager/QuizManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makers.quizmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
