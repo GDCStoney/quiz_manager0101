f1d1112482a6e3796125f5e627c677af7bfbdc03
gstoney2 <gstoney2@ford.com>
2022-01-30 12:11:27 +0000

    Applying the filter in the application - to identify the protected API endpoints - and leaving login and register open

--- src/main/java/com/makers/quizmanager/QuizManagerApiApplication.java
+++ src/main/java/com/makers/quizmanager/QuizManagerApiApplication.java
@@ -1,7 +1,10 @@
 package com.makers.quizmanager;
 
+import com.makers.quizmanager.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuizManagerApiApplication {
@@ -10,4 +13,12 @@ public class QuizManagerApiApplication {
 		SpringApplication.run(QuizManagerApiApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/quizmanager/*");
+		return registrationBean;
+	}
 }
