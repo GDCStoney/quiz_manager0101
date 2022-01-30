fa1273fe30efa82107edd5576b0b7db6dfcf45c8
gstoney2 <gstoney2@ford.com>
2022-01-30 11:20:17 +0000

    Adding the ability to create a token to userResource - with the details needed by the interface to manage user activity

--- src/main/java/com/makers/quizmanager/resources/UserResource.java
+++ src/main/java/com/makers/quizmanager/resources/UserResource.java
@@ -1,12 +1,16 @@
 package com.makers.quizmanager.resources;
 
+import com.makers.quizmanager.Constants;
 import com.makers.quizmanager.domain.User;
 import com.makers.quizmanager.services.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,5 +42,21 @@ public class UserResource {
         Map<String, String> map = new HashMap<>();
         map.put("message", "registered successfully");
         return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+    private Map<String, String> generateJWTToken(User user) {
+        long timestamp = System.currentTimeMillis();
+        String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
+                .setIssuedAt(new Date(timestamp))
+                .setExpiration(new Date(timestamp + Constants.TOKEN_VALIDITY))
+                .claim("userId", user.getUserId())
+                .claim("roleId", user.getRoleId())
+                .claim("firstName", user.getFirstName())
+                .claim("lastname", user.getLastName())
+                .claim("email", user.getEmail())
+                .compact();
+        Map<String, String> map = new HashMap<>();
+        map.put("token", token);
+        return map;
     }
 }
