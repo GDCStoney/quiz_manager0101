b672287a1dcfc484d2a47e3c630c9d31776b993a
gstoney2 <gstoney2@ford.com>
2022-01-30 11:12:39 +0000

    Adding a Constants class for the API token variables

---
+++ src/main/java/com/makers/quizmanager/Constants.java
@@ -0,0 +1,8 @@
+package com.makers.quizmanager;
+
+public class Constants {
+
+    public static final String API_SECRET_KEY = "MakersQuizMananger";
+
+    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
+}
