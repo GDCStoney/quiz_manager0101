8cca65e0f3ffb6060d03ac70575fe7f5bed76651
gstoney2 <gstoney2@ford.com>
2022-01-30 13:58:51 +0000

    Created Domain for Quiz, with required attributes and default constructor / getter and setters

---
+++ src/main/java/com/makers/quizmanager/domain/Quiz.java
@@ -0,0 +1,48 @@
+package com.makers.quizmanager.domain;
+
+public class Quiz {
+
+    private Integer quizId;
+    private String Name;
+    private String description;
+    private Integer numberOfQuestions;
+
+    public Integer getQuizId() {
+        return quizId;
+    }
+
+    public void setQuizId(Integer quizId) {
+        this.quizId = quizId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getNumberOfQuestions() {
+        return numberOfQuestions;
+    }
+
+    public void setNumberOfQuestions(Integer numberOfQuestions) {
+        this.numberOfQuestions = numberOfQuestions;
+    }
+
+    public Quiz(Integer quizId, String name, String description, Integer numberOfQuestions) {
+        this.quizId = quizId;
+        Name = name;
+        this.description = description;
+        this.numberOfQuestions = numberOfQuestions;
+    }
+}
