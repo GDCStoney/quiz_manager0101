fd5ef4ce80f055e6f1391fc034a8e6ae7a02307e
gstoney2 <gstoney2@ford.com>
2022-02-05 10:05:06 +0000

    Included changes for CORS Browser access support

--- src/main/java/com/makers/quizmanager/filters/AuthFilter.java
+++ src/main/java/com/makers/quizmanager/filters/AuthFilter.java
@@ -3,6 +3,8 @@ package com.makers.quizmanager.filters;
 import com.makers.quizmanager.Constants;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -14,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class AuthFilter extends GenericFilterBean {
 
 
@@ -22,28 +25,40 @@ public class AuthFilter extends GenericFilterBean {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 
-        String authHeader = httpServletRequest.getHeader("Authorization");
-        if (authHeader != null) {
-            String[] authHeaderArr = authHeader.split("Bearer ");
-            if (authHeaderArr.length > 1 && authHeaderArr[1] != null) {
-                String token = authHeaderArr[1];
-                try {
-                    Claims claims = Jwts.parser().setSigningKey(Constants.API_SECRET_KEY)
-                            .parseClaimsJws(token).getBody();
-                    httpServletRequest.setAttribute("userId", Integer.parseInt(claims.get("userId").toString()));
-                    httpServletRequest.setAttribute("roleId", Integer.parseInt(claims.get("roleId").toString()));
-                }catch (Exception e) {
-                    httpServletResponse.sendError(HttpStatus.FORBIDDEN.value(), "Invalid or expired token");
+        ((HttpServletResponse) response).setHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) response).setHeader("Access-Control-Allow-Credentials", "true");
+        ((HttpServletResponse) response).setHeader("Access-Control-Allow-Methods",
+                "GET, POST, UPDATE" );
+        ((HttpServletResponse) response).setHeader("Access-Control-Max-Age", "3600");
+        ((HttpServletResponse) response).setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
+
+        if("OPTIONS".equalsIgnoreCase(((HttpServletRequest) request).getMethod())) {
+            ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_OK);
+        } else {
+            String authHeader = ((HttpServletRequest) request).getHeader("Authorization");
+            if (authHeader != null) {
+                String[] authHeaderArr = authHeader.split("Bearer ");
+                if (authHeaderArr.length > 1 && authHeaderArr[1] != null) {
+                    String token = authHeaderArr[1];
+                    try {
+                        Claims claims = Jwts.parser().setSigningKey(Constants.API_SECRET_KEY)
+                                .parseClaimsJws(token).getBody();
+                        httpServletRequest.setAttribute("userId", Integer.parseInt(claims.get("userId").toString()));
+                        httpServletRequest.setAttribute("roleId", Integer.parseInt(claims.get("roleId").toString()));
+                    } catch (Exception e) {
+                        httpServletResponse.sendError(HttpStatus.FORBIDDEN.value(), "Invalid or expired token");
+                        return;
+                    }
+                } else {
+                    httpServletResponse.sendError(HttpStatus.FORBIDDEN.value(), "Authorization token must be Bearer [token]");
                     return;
                 }
             } else {
-                httpServletResponse.sendError(HttpStatus.FORBIDDEN.value(), "Authorization token must be Bearer [token]");
+                httpServletResponse.sendError(HttpStatus.FORBIDDEN.value(), "Authorization token must be provided");
                 return;
             }
-        } else {
-            httpServletResponse.sendError(HttpStatus.FORBIDDEN.value(), "Authorization token must be provided");
-            return;
+            chain.doFilter(httpServletRequest, httpServletResponse);
         }
-        chain.doFilter(httpServletRequest, httpServletResponse);
     }
 }
